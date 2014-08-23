d8ed0056c9603ecced47718e8682fcfd76896b10
Hao Chen <areigna@gmail.com>
2014-08-23 16:57:40 -0700

    check device

--- js/index.js
+++ js/index.js
@@ -21,6 +21,18 @@ function onDeviceReady() {
         // Because we use fixed-through navbar we can enable dynamic navbar
         dynamicNavbar: true
     });
+    navigator.camera.getPicture(onSuccess, onFail, { quality: 50,
+    destinationType: Camera.DestinationType.DATA_URL
+});
+
+function onSuccess(imageData) {
+    var image = document.getElementById('myImage');
+    image.src = "data:image/jpeg;base64," + imageData;
+}
+
+function onFail(message) {
+    alert('Failed because: ' + message);
+}
 }
 
 window.addEventListener("batterystatus", onBatteryStatus, false);
