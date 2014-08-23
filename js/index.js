41364f8dbb5ec4fc0a08001292fc957f234db026
Hao Chen <areigna@gmail.com>
2014-08-23 14:47:44 -0700

    test

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
-alert();
+// Initialize your app
+var ricepo = new Framework7({
+    // Hide and show indicator during ajax requests
+    onAjaxStart: function (xhr) {
+        ricepo.showIndicator();
+    },
+    onAjaxComplete: function (xhr) {
+        ricepo.hideIndicator();
+    }
+});
+
+// Export selectors engine
+var $$ = Dom7;
+
+// Add view
+var mainView = ricepo.addView('.view-main', {
+    // Because we use fixed-through navbar we can enable dynamic navbar
+    dynamicNavbar: true
+});
+
